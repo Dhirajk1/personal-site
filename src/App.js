09661bca5c0017f7c2d84de28cd25b66f277d6c3
Dhiraj Kuttichirayil <dhirajk1@gmail.com>
2022-07-18 19:42:19 -0400

    Adding tailwind integration

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 class="text-blue-600 hover:text-red-300 underline">
+          TESTING TAILWIND
+        </h1>
         Personal Site for Dhiraj Kuttichirayil
       </header>
     </div>

a484fac746b66abcaf134bc4bf4b25a4ff155938
Dhiraj Kuttichirayil <dhirajk1@gmail.com>
2022-07-20 10:12:28 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,32 @@ function App() {
           TESTING TAILWIND
         </h1>
         Personal Site for Dhiraj Kuttichirayil
+        <div class="max-w-sm overflow-hidden shadow-lg bg-slate-200 rounded-lg">
+          <img
+            class="w-full"
+            src="https://cdn.siasat.com/wp-content/uploads/2020/08/panda-1536x1024.jpg"
+            alt="Sunset in the mountains"
+          />
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #photography
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #travel
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #winter
+            </span>
+          </div>
+        </div>
       </header>
     </div>
   );
